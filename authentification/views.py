47074097a426b747ddea6f6fd189e622f8a634ee
rihabelmoutiaf <eylemlay@gmail.com>
2025-05-05 20:10:29 +0100

    Your commit message here

--- authentification/views.py
+++ authentification/views.py
@@ -1,4 +1,15 @@
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
 
-from django.shortcuts import render , HttpResponse 
-def auth(request):   
-  return render(request,'login.html')
+def auth(request):  
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  # Redirect to the homepage after successful login
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'login.html', {'form': form})
