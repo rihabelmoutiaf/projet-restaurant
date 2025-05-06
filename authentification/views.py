e875ba740efb88569716fac64651c5dcf96d36b4
rihabelmoutiaf <eylemlay@gmail.com>
2025-05-06 19:16:02 +0100

    Your commit message here

--- authentification/views.py
+++ authentification/views.py
@@ -1,15 +1,17 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm
 
-def auth(request):  
+def auth(request):
+    error = None 
     if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('home')  # Redirect to the homepage after successful login
-    else:
-        form = AuthenticationForm()
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
-    return render(request, 'login.html', {'form': form})
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            error = "Identifiants invalides."  
+
+    return render(request, 'login.html', {'error': error})
